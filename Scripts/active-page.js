46356958c7b7897316dd23027102816bd9de1646
Anoke <88586442+Anoke@users.noreply.github.com>
2023-12-05 01:23:28 +0300

    lab-5

---
+++ Scripts/active-page.js
@@ -0,0 +1,26 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     let location = window.location.href;
+//     let cur_url = location.split('/').pop();
+// console.log(cur_url)
+//     let menuItems = document.querySelectorAll('.header-menu__site-navigation ul li, .header-menu__middle ul li ');
+//     menuItems.forEach(function(item) {
+//         let link = item.querySelector('a').getAttribute('href');
+// console.log(link)
+//         console.log(cur_url===link)
+//         if (cur_url === link) {
+//             item.classList.add('active');
+//         }
+//     });
+// });
+
+window.addEventListener('load', () => {
+    let location = window.location.href;
+    let cur_url = location.split('/').pop();
+    let menuItems = document.querySelectorAll('.header-menu__site-navigation ul li, .header-menu__middle ul .cart, .header-menu__basic-info ul .header-menu__basic-info__registration-entry, .header-menu__middle ul .postponed');
+    menuItems.forEach(function (item){
+        let link = item.querySelector('a').getAttribute('href');
+        if (cur_url===link){
+            item.classList.add('active')
+        }
+    })
+})
