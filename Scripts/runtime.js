0169e028a1bd752f4613484e1fedccacd1522735
Anoke <88586442+Anoke@users.noreply.github.com>
2023-12-05 10:22:37 +0300

    lab-5.1

--- Scripts/runtime.js
+++ Scripts/runtime.js
@@ -1,104 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const form = document.getElementById('form');
-//     const resultContainer = document.getElementById('resultContainer');
-//
-//     form.addEventListener('submit', (event) => {
-//         event.preventDefault(); // Предотвращаем перезагрузку страницы
-//
-//         // Получаем значения из формы
-//         const maxLessons = document.getElementById('maxLessons').value;
-//         const language = document.getElementById('language').value;
-//         const weekType = document.getElementById('weekType').value;
-//
-//         // Генерация результата (можно заменить на генерацию таблицы или карточек)
-//         const result = `
-//       <p>Max Lessons: ${maxLessons}</p>
-//       <p>Language: ${language}</p>
-//       <p>Week Type: ${weekType === 'fiveDay' ? 'Five-day week' : 'Six-day week'}</p>
-//     `;
-//
-//         resultContainer.innerHTML = result;
-//
-//         // Сохранение данных в локальное хранилище
-//         const formData   = {
-//             maxLessons,
-//             language,
-//             weekType
-//         };
-//         localStorage.setItem('formData', JSON.stringify(formData));
-//     });
-//
-//     // Загрузка данных из локального хранилища при загрузке страницы
-//     const savedFormData = localStorage.getItem('formData');
-//     if (savedFormData) {
-//         const parsedData = JSON.parse(savedFormData);
-//         document.getElementById('maxLessons').value = parsedData.maxLessons;
-//         document.getElementById('language').value = parsedData.language;
-//         document.getElementById('weekType').value = parsedData.weekType;
-//     }
-// });
-// document.addEventListener('DOMContentLoaded', () => {
-//     const noteForm = document.getElementById('noteForm');
-//     const noteInput = document.getElementById('noteInput');
-//     const noteList = document.getElementById('noteList');
-//
-//     // Загрузка заметок из localStorage при загрузке страницы
-//     const savedNotes = JSON.parse(localStorage.getItem('notes')) || [];
-//     savedNotes.forEach(note => addNoteToList(note.text, note.completed));
-//
-//     // Функция добавления заметки в список
-//     function addNoteToList(noteText, completed = false) {
-//         const listItem = document.createElement('li');
-//         listItem.textContent = noteText;
-//         const deleteButton = document.createElement('button');
-//         deleteButton.textContent = 'delete';
-//         deleteButton.classList.add('deleteButton');
-//         listItem.appendChild(deleteButton);
-//         noteList.appendChild(listItem);
-//
-//         if (completed) {
-//             listItem.classList.add('completed');
-//         }
-//
-//         // Добавление обработчика события для зачеркивания текста по клику
-//         listItem.addEventListener('click', () => {
-//             listItem.classList.toggle('completed');
-//             updateLocalStorage();
-//         });
-//
-//         // Добавление обработчика события для удаления заметки
-//         deleteButton.addEventListener('click', () => {
-//             listItem.remove();
-//             updateLocalStorage();
-//         });
-//
-//         // Сохраняем заметку в localStorage
-//         updateLocalStorage();
-//     }
-//
-//     // Обработка отправки формы для добавления заметки
-//     noteForm.addEventListener('submit', event => {
-//         event.preventDefault(); // Предотвращаем стандартное поведение формы
-//
-//         const noteText = noteInput.value.trim();
-//         if (noteText !== '') {
-//             addNoteToList(noteText);
-//             noteInput.value = ''; // Очищаем поле ввода
-//         }
-//     });
-//
-//     // Обновление данных в localStorage
-//     function updateLocalStorage() {
-//         const notes = [];
-//         document.querySelectorAll('#noteList li').forEach(note => {
-//             notes.push({
-//                 text: note.textContent,
-//                 completed: note.classList.contains('completed')
-//             });
-//         });
-//         localStorage.setItem('notes', JSON.stringify(notes));
-//     }
-// });
 document.addEventListener('DOMContentLoaded', () => {
     const noteForm = document.getElementById('noteForm');
     const noteInput = document.getElementById('noteInput');
@@ -135,7 +34,6 @@ document.addEventListener('DOMContentLoaded', () => {
             updateCategories();
         });
 
-        // Добавление обработчика события для зачеркивания текста по клику на span
         noteSpan.addEventListener('click', () => {
             noteElement.classList.toggle('completed');
             updateLocalStorage();
@@ -166,10 +64,8 @@ document.addEventListener('DOMContentLoaded', () => {
         allTasksElement.textContent = `${allTasks}`;
     }
 
-    // Load notes from local storage on page load
     const savedNotes = JSON.parse(localStorage.getItem('notes')) || [];
     savedNotes.forEach(note => addNoteToList(note.text, note.completed));
 
-    // Update categories on page load
     updateCategories();
 });
